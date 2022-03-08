7b95dcfb6019786b2379ad1b54fa97352ebe0cbc
jingchengpeng@yahoo.com <jingchengpeng@yahoo.com>
2022-03-08 01:02:05 -0500

    Made some progress on CRUD

--- app.js
+++ app.js
@@ -21,16 +21,33 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
-
+//
+app.use(express.urlencoded({extended: true}))
 
 
 app.get('/', (req, res) => {
     res.render('home')
 })
+// this request show all the items;
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', {campgrounds})
 })
+// new campground;
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new')
+})
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save()
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+// show the campground;
+app.get('/campgrounds/:id', async(req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', {campground})
+})
+
 
 
 
--- seeds/index.js
+++ seeds/index.js
@@ -4,7 +4,6 @@ const cities = require('./cities');
 const {places, descriptors} = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     useUnifiedTopology: true
